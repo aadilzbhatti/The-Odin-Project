2432848c7fe359735fcd4e9617a6664b26a6e35c
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-06-05 19:32:40 -0500

    guessed letters
    Added a guessed letters component

--- RubyProjects/Hangman/hangman.rb
+++ RubyProjects/Hangman/hangman.rb
@@ -8,6 +8,14 @@ def make_guess_word(word)
   guess_word
 end
 
+def guess_amt(guesses)
+  if guesses > 1
+    "You have #{guesses} guesses left"
+  else
+    "You have #{guesses} guess left"
+  end
+end
+
 def check_guess(word, guess)
   word.include?guess
 end
@@ -21,8 +29,8 @@ def update_guess_word(word, guess_word, guess)
   guess_word
 end
 
-def save_game(guesses, guess_word, word)
-  data = [guesses, guess_word, word]
+def save_game(guesses, guess_word, word, guessed_letters)
+  data = [guesses, guess_word, word, guessed_letters]
   save_data = YAML::dump(data)
   File.open('save_data.yaml', 'w') do |f|
     f.puts save_data
@@ -44,6 +52,7 @@ if input == 'YES'
   guesses = load_game[0]
   guess_word = load_game[1]
   word = load_game[2]
+  guessed_letters = load_game[3]
 else
   text = File.readlines('text.txt')
   dict = text.map {|line| line.chomp}.select {|line| line.length >= 5 && line.length <= 12 }
@@ -52,18 +61,21 @@ else
   guess_word = make_guess_word(word)
 
   guesses = 8
+  guessed_letters = []
 end
 
 victory = false
 
 while guesses > 0 do
-  puts "You have #{guesses} guesses left"
+  puts guess_amt(guesses)
   puts "The word now looks like this: #{guess_word}"
+  puts "You have guessed #{guessed_letters}"
 
   puts %q{Would you like to save the game? Type 'yes' if you would like to, otherwise type any other key.}
   input = gets.chomp.upcase
   if input == 'YES'
-    save_game(guesses, guess_word, word)
+    save_game(guesses, guess_word, word, guessed_letters)
+    exit
   end
 
   puts 'Enter your guess below: '
@@ -71,6 +83,7 @@ while guesses > 0 do
   if check_guess(word, guess)
     guess_word = update_guess_word(word, guess_word, guess)
   else
+    guessed_letters << guess
     guesses-=1
   end
   if guess_word == word
